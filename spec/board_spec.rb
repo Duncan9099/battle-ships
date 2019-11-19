c125605c184f65cd689c92d1782cc2e7015b1020
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-19 13:36:10 +0000

    starting battleships test

---
+++ spec/board_spec.rb
@@ -0,0 +1,10 @@
+require '../lib/board'
+
+describe 'board' do
+  subject(:board) { Board.new }
+  describe '#initialize' do
+    it 'has an array of 100 upon initialization' do
+
+    end
+  end
+end
